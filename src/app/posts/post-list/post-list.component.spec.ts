53cb6d6b7e51d232d91381d68407d23c269aa484
Yunus YILMAZ <yunusyilmazz34@gmail.com>
2023-03-08 22:00:34 +0300

    added material ui, service and rxjs.

--- src/app/posts/post-list/post-list.component.spec.ts
+++ src/app/posts/post-list/post-list.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PostsComponent } from './posts.component';
+import { PostListComponent } from './post-list.component';
 
-describe('PostsComponent', () => {
-  let component: PostsComponent;
-  let fixture: ComponentFixture<PostsComponent>;
+describe('PostListComponent', () => {
+  let component: PostListComponent;
+  let fixture: ComponentFixture<PostListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PostsComponent ]
+      declarations: [ PostListComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(PostsComponent);
+    fixture = TestBed.createComponent(PostListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/posts/posts.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-posts',
-  templateUrl: './posts.component.html',
-  styles: [],
-})
-export class PostsComponent {}
