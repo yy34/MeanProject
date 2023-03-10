fdc5d73eaf50e96f8311a6c741bfb3829ccf7358
Yunus YILMAZ <yunusyilmazz34@gmail.com>
2023-03-10 20:58:33 +0300

    added express.js and cors headers

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,47 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false })); //* optional
+
 app.use((req, res, next) => {
-  console.log("Express test 1");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
   next();
 });
 
-app.use((req, res, next) => {
-  res.send("Express test 2");
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  res.status(201).json({
+    message: "Post added successfully",
+  });
+});
+
+app.get("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "nkjn12912j4md",
+      title: "Mock Title 1",
+      content: "Mock Content 1",
+    },
+    {
+      id: "mtamdewkk23",
+      title: "Mock Title 2",
+      content: "Mock Content2",
+    },
+  ];
+  res.status(200).json({
+    message: "Posts fetched successfully!",
+    posts: posts,
+  });
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const onError = (error) => {
   if (error.syscall !== "listen") {
     throw error;
   }
-  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
   switch (error.code) {
     case "EACCES":
       console.error(bind + " requires elevated privileges");
@@ -37,7 +37,7 @@ const onError = (error) => {
 
 const onListening = () => {
   const addr = server.address();
-  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
   debug("Listening on " + bind);
 };
 
