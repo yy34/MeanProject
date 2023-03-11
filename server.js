e6f43db1eb418dd2e4c6279d28c10964125c5530
Yunus YILMAZ <yunusyilmazz34@gmail.com>
2023-03-11 11:51:35 +0300

    added mongodb

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 const app = require("./backend/app");
 const debug = require("debug")("node-angular");
 const http = require("http");
-
 const normalizePort = (val) => {
   var port = parseInt(val, 10);
-
   if (isNaN(port)) {
     return val;
   }
-
   if (port >= 0) {
     return port;
   }
-
   return false;
 };
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -29,6 +30,7 @@ import { PostListComponent } from './posts/post-list/post-list.component';
     MatButtonModule,
     MatToolbarModule,
     MatExpansionModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
